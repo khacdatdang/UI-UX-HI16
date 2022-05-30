179ca6f7cb766762687fcc944c4fa2a08fef39d0
minh0612 <vungocminh112233@gmail.com>
2022-05-30 14:51:46 +0700

    add kandan

--- src/component/dashpage/dashpage.js
+++ src/component/dashpage/dashpage.js
@@ -3,9 +3,122 @@ import { Doughnut } from "react-chartjs-2";
 import {Chart, ArcElement} from 'chart.js';
 import RemoveOutlinedIcon from '@mui/icons-material/RemoveOutlined';
 import AddBoxOutlinedIcon from '@mui/icons-material/AddBoxOutlined';
-import Board from 'react-trello'
 
 import './dashpage.scss';
+import AppA from "./UncontrolledBoard";
+
+import { useState } from "react";
+import ReactDOM from "react-dom";
+import Board, { moveCard } from "@lourenci/react-kanban";
+import "@lourenci/react-kanban/dist/styles.css";
+// Use your own styles to override the default styles
+// import "./styles.css";
+
+const board = {
+  columns: [
+    {
+      id: 1,
+      title: "Backlog",
+      backgroundColor: "#fff",
+      cards: [
+        {
+          id: 1,
+          title: "Card title 1",
+          description: "Card content"
+        },
+        {
+          id: 2,
+          title: "Card title 2",
+          description: "Card content"
+        },
+        {
+          id: 3,
+          title: "Card title 3",
+          description: "Card content"
+        }
+      ]
+    },
+    {
+      id: 2,
+      title: "Doing",
+      cards: [
+        {
+          id: 9,
+          title: "Card title 9",
+          description: "Card content"
+        }
+      ]
+    },
+    {
+      id: 3,
+      title: "Q&A",
+      cards: [
+        {
+          id: 10,
+          title: "Card title 10",
+          description: "Card content"
+        },
+        {
+          id: 11,
+          title: "Card title 11",
+          description: "Card content"
+        }
+      ]
+    },
+    {
+      id: 4,
+      title: "Production",
+      cards: [
+        {
+          id: 12,
+          title: "Card title 12",
+          description: "Card content"
+        },
+        {
+          id: 13,
+          title: "Card title 13",
+          description: "Card content"
+        }
+      ]
+    }
+  ]
+};
+
+function ControlledBoard() {
+  // You need to control the state yourself.
+  const [controlledBoard, setBoard] = useState(board);
+
+  function handleCardMove(_card, source, destination) {
+    const updatedBoard = moveCard(controlledBoard, source, destination);
+    setBoard(updatedBoard);
+  }
+
+  return (
+    <Board onCardDragEnd={handleCardMove} disableColumnDrag>
+      {controlledBoard}
+    </Board>
+  );
+}
+
+function UncontrolledBoard() {
+  return (
+    <Board
+      allowRemoveLane
+      allowRenameColumn
+      allowRemoveCard
+      onLaneRemove={console.log}
+      onCardRemove={console.log}
+      onLaneRename={console.log}
+      initialBoard={board}
+      allowAddCard={{ on: "top" }}
+      onNewCardConfirm={(draftCard) => ({
+        id: new Date().getTime(),
+        ...draftCard
+      })}
+      onCardNew={console.log}
+    />
+  );
+}
 
 
 Chart.register(ArcElement);
@@ -13,9 +126,6 @@ Chart.register(ArcElement);
 
 
 function DashPage() {
-
-    
-
     return(
         <div className="dashpage">
             <div className="dashpage-header">
@@ -106,6 +216,8 @@ function DashPage() {
                     </div>
                 </div>
             </div>
+            <UncontrolledBoard />
+            <ControlledBoard />
         </div>
     );
 }
